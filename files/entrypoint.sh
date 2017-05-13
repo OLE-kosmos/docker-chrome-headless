6fe07f50e4359df02db48b4109714a9eab40f969
Dorian More <doriancmore@gmail.com>
2017-05-13 13:23:07 +0300

    Allow environment variable to configure workdir

--- files/entrypoint.sh
+++ files/entrypoint.sh
@@ -2,6 +2,10 @@
 
 umask 000
 
+if [[ "${DOCROOT}" != "" ]]; then
+    sed -i "s#root /code/#root ${DOCROOT}/#" /etc/nginx/sites-enabled/mink
+fi
+
 /usr/bin/supervisord -n -c /etc/supervisord.conf > /dev/null 2>&1 &
 
 if [[ $# -eq 1 && $1 == "bash" ]]; then
